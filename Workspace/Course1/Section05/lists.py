4b99510f96bd4b0851304331eb4608411d862bfb
VYV7 <peter.szwajda@gmail.com>
2022-03-10 10:06:26 +0000

    Latest Changes from E7470

--- Workspace/Course1/Section05/lists.py
+++ Workspace/Course1/Section05/lists.py
@@ -1,7 +1,13 @@
 print("========================================================================")
+# https://docs.python.org/3/tutorial/datastructures.html
+
 mylist = []
 
-s = "something"
+s = "something big"
+print(s)
+
+print(s.count("i"))
+
 
 print(mylist)
 for letter in s:
@@ -9,7 +15,14 @@ for letter in s:
 	print(letter)
 print(mylist)
 
+mylist.append("!")
+print(mylist)
 
+mylist.reverse()
+print(mylist)
+
+
+print("\n-------------------------------------------------")
 # list comprehension format
 newlist = [letter for letter in s]
 print(newlist)
@@ -22,5 +35,12 @@ print(evens)
 
 mylist = [x if x%2 == 0 else 'not even' for x in range(0,11)]
 print(mylist)
+
+print("\n-------------------------------------------------")
+
+l = [x*2 for x in range(0,10)]
+print(l)
+
+
 
 print("========================================================================")
